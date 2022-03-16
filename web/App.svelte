de9dd7be094158f1a79a2a12baaa742f857f7efd
Trevor <trevor@osrs-stat.com>
2022-03-16 14:01:55 +0900

    Set up initial app layout

--- web/App.svelte
+++ web/App.svelte
@@ -1,30 +1,69 @@
 <script lang="ts">
-	export let name: string;
+	/* hey, wanna hire me? */
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<section>
+	   <h1>draw32</h1>
+   </section>
+
+  <section>
+    <h2>Canvas</h2>
+  </section>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    box-sizing: border-box;
+    padding: 0.75em;
+    height: 100%;
+    text-align: center;
+    max-width: 240px;
+    margin: 0;
+    border: 1px solid black;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    /* Grid */
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: 1fr;
+    column-gap: 2.25em;
+    row-gap: normal;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: slateblue;
+    text-transform: uppercase;
+    font-size: 3em;
+    font-weight: 100;
+    margin-top: 0px;
+    margin-bottom: 1rem;
+  }
+
+  h2 {
+    font-weight: 100;
+    margin-top: 0px;
+  }
+
+  section {
+    background-color: #eee; /* confirm layout */
+    height: inherit;
+    max-height: 100vh;
+  }
+
+  @media (min-width: 240px) {
+    main { max-width: none; }
+  }
+
+  @media (min-width: 370px) { /* small idevice */
+    section { min-width: 185px; }
+  }
+
+  @media (max-width: 640px) { /* swap to col mode here */
+    main {
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr 2fr;
+      column-gap: normal;
+      row-gap: 2.25em;
+    }
+  }
 </style>
--- web/main.ts
+++ web/main.ts
@@ -2,9 +2,7 @@ import App from './App.svelte';
 
 const app = new App({
 	target: document.body,
-	props: {
-		name: 'w̶o̶r̶l̶d̶ potential employers; please hire me'
-	}
+	props: {}
 });
 
 export default app;
